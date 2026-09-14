bff2131b90ee7b45529e1ac43ee69e2cf00100bb
Park SeHun <103345490+david8575@users.noreply.github.com>
2026-09-14 23:00:24 +0900

    Time: 552 ms (56.29%), Space: 21.7 MB (78.42%) - LeetHub

---
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def maxOperations(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        nums.sort()
+        left, right = 0, len(nums)-1
+        cnt = 0
+
+        while left < right:
+            if nums[left] + nums[right] == k:
+                cnt+=1
+                left+=1
+                right-=1
+            elif nums[left] + nums[right] > k:
+                right-=1
+            else:
+                left+=1
+        return cnt
